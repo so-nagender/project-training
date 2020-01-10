1ee7ab64fcc68f6a77a0edcf312ff85d01000dfd
ShubhamAgrahariOncehub <shubham.agrahari@oncehub.com>
2020-01-10 19:31:48 +0530

    I learnt loops and some basics of Java Script

---
+++ Shubham-Agrahari/daily-learning-material/functionNew.js
@@ -0,0 +1,12 @@
+/* let Multiply = function(num1,num2){
+    return  num1 * num2
+   
+}
+console.log(Multiply(3,8))
+*/
+
+let guestUser = function(name = "unName",coarsecount = 0){
+    return "Hello " + name + " Your coarse count is " + coarsecount 
+    
+}
+console.log(guestUser('Shubham','19'))
