a65c399e75f8a2207d7768593428e658c03b37e9
monika-gupta <monika.gupta@oncehub.com>
2020-01-16 13:13:10 +0530

    final submission of first assignment

--- monika-gupta/html-stuff/firstAssignment/javascript/script.js
+++ monika-gupta/html-stuff/firstAssignment/javascript/script.js
@@ -1,15 +1,16 @@
+//open pop-up
 function openpopup() {
     document.getElementById('popup').style.display = 'block';
 }
+//close pop-up
 function closepopup() {
     document.getElementById('popup').style.display = 'none';
 }
 var table = document.createElement('table');
-function tableHeader()
-{
+// header function
+function tableHeader() {
     var customers = new Array();
     customers.push(["Firstname", "Lastname","Gender","Contact-No.","Email","Address","Country","Skills"]);
-   
     var columnCount = customers[0].length;
     var row = table.insertRow(-1);
     for (var i = 0; i < columnCount; i++) {
@@ -18,46 +19,54 @@ function tableHeader()
         row.appendChild(headerCell);
     }
 }
-tableHeader()
-function generateTable() {
+//calling header
+tableHeader();
+
+var form = document.getElementById("popup");
+form.addEventListener('submit',generateTable);
+
+function generateTable(event) {
+    event.preventDefault();
+    console.log('event',event);
+
     var customers = new Array();
     var gender;
-    var text= "";
+    var text = "";
 
     var firstName = document.getElementById("firstname");
-    var lastName= document.getElementById("lastname");
+    var lastName = document.getElementById("lastname");
 
     var gender2 = document.getElementsByClassName("gender")
-    for(let i =0;i<=2;i++){
-        if(gender2[i].checked){
+    for(let i = 0;i <= 2;i++) {
+        if(gender2[i].checked) {
             gender = gender2[i];
 
         }    
     }
     
     var contactNo = document.getElementById("contactno");
-    var email= document.getElementById("email");
+    var email = document.getElementById("email");
     var address = document.getElementById("textarea");
     var country = document.getElementById("country");
 
     var skills = document.getElementsByClassName("skill");
-    for(let i =0;i<=4;i++){
+    for(let i = 0;i <= 4;i++){
         if(skills[i].checked){
-            text = text + "," + skills[i].value;
+            text += skills[i].value + "<br>";
 
         }    
     }
         
     var row = table.insertRow();
      
-    row.insertCell(0).innerHTML= firstName.value;
-    row.insertCell(1).innerHTML= lastName.value;
-    row.insertCell(2).innerHTML= gender.value;
-    row.insertCell(3).innerHTML= contactNo.value;
-    row.insertCell(4).innerHTML= email.value;
-    row.insertCell(5).innerHTML= address.value;
-    row.insertCell(6).innerHTML= country.value;
-    row.insertCell(7).innerHTML= text;
+    row.insertCell(0).innerHTML = firstName.value;
+    row.insertCell(1).innerHTML = lastName.value;
+    row.insertCell(2).innerHTML = gender.value;
+    row.insertCell(3).innerHTML = contactNo.value;
+    row.insertCell(4).innerHTML = email.value;
+    row.insertCell(5).innerHTML = address.value;
+    row.insertCell(6).innerHTML = country.value;
+    row.insertCell(7).innerHTML = text;
      
     var dvTable = document.getElementById("generateTable");
     dvTable.innerHTML = "";
