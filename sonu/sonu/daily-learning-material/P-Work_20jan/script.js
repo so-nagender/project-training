22acaa3a516b9ebd109e06dbc879eae370d02bb0
sonu kumar <sonu.sah@oncehub.com>
2020-01-20 18:54:58 +0530

    dashboard

---
+++ sonu/sonu/daily-learning-material/P-Work_20jan/script.js
@@ -0,0 +1,13 @@
+function signMeUp(){
+    localStorage.setItem("name","sonu kumar");
+    localStorage.setItem("email","1997sonukumarsah@gmail.com");
+    localStorage.setItem("password","13579")
+}
+
+function signMeIn(){
+  const name =  localStorage.getItem("name");
+  const email =  localStorage.getItem("email");
+  const password =  localStorage.getItem("password");
+
+}
+
