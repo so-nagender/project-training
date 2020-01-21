61cf408603bcb7a4b9ce1843bb04748dd67351b5
sonu kumar <sonu.sah@oncehub.com>
2020-01-21 18:35:29 +0530

    cookie addition

---
+++ sonu/sonu/daily-learning-material/P-Work_21jan/dsscript.js
@@ -0,0 +1,18 @@
+function applyPage(){
+    localStorage.setItem("f_name","sonu kumar")
+    localStorage.setItem("l_name","sah")
+    localStorage.setItem("email","500055344@stu.upes.ac.in")
+    localStorage.setItem("amount","400000")
+    localStorage.setItem("bank_acc_no","33129687902")
+    localStorage.setItem("acc_holder_name","sonu kumar sah")
+}
+
+// function getApplyPages()
+// {
+//     const f_name = localStorage.getItem("f_name");
+//     const l_name = localStorage.getItem("l_name");
+//     const email = localStorage.getItem("email");
+//     const amount = localStorage.getItem("amount");
+//     const bank_acc_no= localStorage.getItem("bank_acc_no");
+//     const acc_holder_name = localStorage.getItem("acc_holder_name");
+// }
---
+++ sonu/sonu/daily-learning-material/P-Work_21jan/script.js
@@ -0,0 +1,48 @@
+var x=document.getElementById("signup");
+  x.addEventListener("submit",addcookie);
+  function addcookie(event)
+  {
+   var cname=event.target.elements[1].value+event.target.elements[2].value ;
+   var cvalue=false;
+   var exdays=10;
+   setCookie(cname,cvalue,exdays);  
+  }
+
+function setCookie(cname,cvalue,exdays) {
+  event.preventDefault();
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires=" + d.toGMTString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  alert("added")
+}
+
+
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie() {
+  var x=document.getElementsByClassName("form-control");
+  var username=x[0].value+x[1].value;
+  var user=getCookie(username);
+  if (user != "") {
+    window.open("http://127.0.0.1:5500/dashboard.html", "_blank");
+  } else {
+     alert("username incorrect");
+  }
+}
+
