5c4efdeb09c2727342a20298b43140dfa56ae674
shubhamoncehub <shubham.kumar@oncehub.com>
2020-01-27 18:20:16 +0530

    angular-bookstore-template

---
+++ Shubham-kumar/angular/newapp/src/app/body-right/body-right.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-body-right',
+  templateUrl: './body-right.component.html',
+  styleUrls: ['./body-right.component.css']
+})
+export class BodyRightComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
