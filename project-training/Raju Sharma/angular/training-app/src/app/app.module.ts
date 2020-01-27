c497303ae524bbb6ac00d3cc4592fc2ce5596349
Raju Sharma <raju.sharma@oncehub.com>
2020-01-27 18:20:22 +0530

    Angular 1st project

---
+++ project-training/Raju Sharma/angular/training-app/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyLeftComponent } from './body-left/body-left.component';
+import { BodyRightComponent } from './body-right/body-right.component';
+import { FooterComponent } from './footer/footer.component';
+import { ExtraComponent } from './extra/extra.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    BodyLeftComponent,
+    BodyRightComponent,
+    FooterComponent,
+    ExtraComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
