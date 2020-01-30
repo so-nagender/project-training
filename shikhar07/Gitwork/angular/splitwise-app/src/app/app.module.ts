b48dd3719db316fbd39ac2823f2bc65a97798c09
gupta-shikhar07 <shikhar.gupta@oncehub.com>
2020-01-30 18:58:20 +0530

    The project webpage developed in html is integrated in Angular

---
+++ shikhar07/Gitwork/angular/splitwise-app/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MiddleSectionComponent } from './middle-section/middle-section.component';
+import { FooterSectionComponent } from './footer-section/footer-section.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MiddleSectionComponent,
+    FooterSectionComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
