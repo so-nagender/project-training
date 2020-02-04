d9c2305286583bdef303e696613e48deea559492
sonu kumar <sonu.sah@oncehub.com>
2020-02-04 15:52:45 +0530

    added angular form

---
+++ sonu/Angular/angular-form/src/app/share/confirm-equal-validator.directive.ts
@@ -0,0 +1,21 @@
+import { Validators , NG_VALIDATORS, AbstractControl } from '@angular/forms';
+import { Directive, Input } from '@angular/core';
+
+@Directive({
+   selector: '[appConfirmEqualValidor]',
+   providers: [{
+       provide: NG_VALIDATORS,
+       useExisting: ConfirmEqualValidatorDirective,
+       multi: true
+   }]
+})
+export class ConfirmEqualValidatorDirective implements Validators {
+    @Input appConfirmEqualValidor: string;
+    validate(control: AbstractControl) { [key: String]: any} | null {
+    const controlToCompare = control.parent.get(this.appConfirmEqualValidor);
+    if(controlToCompare && controlToCompare.value  !== control.value) {
+        return { 'notEqual': true };
+    }
+        return null;
+    }
+}
