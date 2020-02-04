ec58ba22bf37c711f57eea84ee043c784bc782f8
nishant-fsd <nishant.kumar@oncehub.com>
2020-02-04 16:32:45 +0530

    klv

--- nishant-fsd/angular/angproject/src/app/app.module.ts
+++ nishant-fsd/angular/angproject/src/app/app.module.ts
@@ -6,10 +6,18 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BannerComponent } from './banner/banner.component';
 import { FooterComponent } from './footer/footer.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './form/form.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+
+const appRoutes: Routes = [
+  { path: 'form', component: FormComponent },
+  {path:'sign-in',component:SignInComponent}
+];
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, BannerComponent, FooterComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [AppComponent, HeaderComponent, BannerComponent, FooterComponent, FormComponent, SignInComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule,ReactiveFormsModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
 })
