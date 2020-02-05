a2a5c60b5c4c5b18e960003ac387aac67dbe94ad
shiwaniOncehub <shiwani.bhardwaj@oncehub.com>
2020-02-05 12:54:17 +0530

    Angular Form Validation Completed

--- shiwani-bhardwaj/angular/burger1/src/app/main1/main1.component.ts
+++ shiwani-bhardwaj/angular/burger1/src/app/main1/main1.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+ function item(value)
+{
+  this.value=value;
+}
+item.prototype.add =function(){
+  this.value ++;
+}
+item.prototype.remove =function(){
+  this.value --;
+}
 
 @Component({
   selector: 'app-main1',
@@ -6,10 +16,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main1.component.css']
 })
 export class Main1Component implements OnInit {
+  burger = { value:0 };
+  pizza = { value:0 };
+  sandwich = { value:0 };
 
   constructor() { }
-
+ 
   ngOnInit() {
   }
-
-}
+  add(item)
+  {
+   item.value ++;
+  }
+  remove(item)
+  {
+    item.value --;
+  }
+  }
