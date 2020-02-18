76dcd4fe8df200e1d8e78f333317ebe08871e9a8
sonu kumar <sonu.sah@oncehub.com>
2020-02-18 16:22:35 +0530

    signup and signin form validation

--- book-store/frontend/src/app/login/login.component.ts
+++ book-store/frontend/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form=new FormGroup({
+    email: new FormControl('',[Validators.required, Validators.email]),
+    password: new FormControl('',[Validators.required,Validators.minLength(6)])
+  })
 
   constructor() { }
 
--- book-store/frontend/src/app/signup/signup.component.ts
+++ book-store/frontend/src/app/signup/signup.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder, EmailValidator } from '@angular/forms';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
+
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
+
+
 export class SignupComponent implements OnInit {
+  form = new FormGroup({
+    fname: new FormControl('',Validators.required),
+    lname: new FormControl('',Validators.required),
+    email: new FormControl('',[Validators.required, Validators.email]),
+    password: new FormControl('',[Validators.required, Validators.minLength(6)]),
+    password2: new FormControl('',[Validators.required, Validators.minLength(6)])
+  })
 
   constructor() { }
+ 
 
   ngOnInit() {
   }
 
+  
+  
+ 
+
+  
 }
