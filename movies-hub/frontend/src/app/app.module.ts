76f250b2543e64b440e4f559431aae5f9dd64166
Raju Sharma <raju.sharma@oncehub.com>
2020-02-18 11:16:26 +0530

    component

--- movies-hub/frontend/src/app/app.module.ts
+++ movies-hub/frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DefaultComponent } from './default/default.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
