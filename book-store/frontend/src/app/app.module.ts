eab62876bdd4fe038abbb12eb1c433317ee70fd9
shubhamoncehub <shubham.kumar@oncehub.com>
2020-02-18 17:51:47 +0530

    category component created

--- book-store/frontend/src/app/app.module.ts
+++ book-store/frontend/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+<<<<<<< Updated upstream
 
 @NgModule({
   declarations: [
@@ -11,6 +12,35 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule
+=======
+import { HomepageComponent } from './homepage/homepage.component';
+import { HeaderComponent } from './header/header.component';
+import { RouterModule, Routes } from '@angular/router';
+import { CategoriesComponent } from './categories/categories.component';
+const appRoutes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'categories', component: CategoriesComponent }
+ 
+  
+];
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomepageComponent,
+    HeaderComponent,
+    CategoriesComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+    // other imports here
+  
+
+>>>>>>> Stashed changes
   ],
   providers: [],
   bootstrap: [AppComponent]
