3f1ca9814807826cb499c8bb380b3c7df648ad94
gupta-shikhar07 <shikhar.gupta@oncehub.com>
2020-02-18 11:26:17 +0530

    Header component

--- movies-hub/frontend/src/app/app.module.ts
+++ movies-hub/frontend/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
