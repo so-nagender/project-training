54c4c6329036b0d72223fcc983f1b537cae74e21
Raju Sharma <raju.sharma@oncehub.com>
2020-02-18 16:29:34 +0530

    default-component done

--- movies-hub/frontend/src/app/app.module.ts
+++ movies-hub/frontend/src/app/app.module.ts
@@ -4,12 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { DefaultComponent } from './default/default.component'
+import { FooterComponent } from './footer/footer.component'
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    DefaultComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
