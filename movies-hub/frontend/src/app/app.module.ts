7b59766a170a61603eaf5db24e83b512673adf4d
Raju Sharma <raju.sharma@oncehub.com>
2020-02-18 17:36:08 +0530

    modification

--- movies-hub/frontend/src/app/app.module.ts
+++ movies-hub/frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
@@ -8,6 +8,10 @@ import { DefaultComponent } from './default/default.component'
 import { FooterComponent } from './footer/footer.component'
 import { from } from 'rxjs';
 
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', component: DefaultComponent },
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,7 +21,10 @@ import { from } from 'rxjs';
     FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
