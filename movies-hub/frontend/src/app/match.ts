d5a6af6b856d5a4df0c4286ee1121beb4b769ee3
Raju Sharma <raju.sharma@oncehub.com>
2020-02-19 10:57:15 +0530

    modi

--- movies-hub/frontend/src/app/match.ts
+++ movies-hub/frontend/src/app/match.ts
@@ -1,14 +1,14 @@
-// import { FormGroup } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 
-// export function MustMatch(email1: string, email2: string) {
-//   // Validating Email match function...(Customize validator.)
-//   return (formGroup: FormGroup) => {
-//     const e = formGroup.controls[email1];
-//     const e2 = formGroup.controls[email2];
-//     if (e.value !== e2.value) {
-//       e2.setErrors({ MustMatch: true });
-//     } else {
-//       e2.setErrors(null);
-//     }
-//   };
-// }
+export function MustMatch(email1: string, email2: string) {
+  // Validating Email match function...(Customize validator.)
+  return (formGroup: FormGroup) => {
+    const e = formGroup.controls[email1];
+    const e2 = formGroup.controls[email2];
+    if (e.value !== e2.value) {
+      e2.setErrors({ MustMatch: true });
+    } else {
+      e2.setErrors(null);
+    }
+  };
+}
