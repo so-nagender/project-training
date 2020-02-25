72e38e04034f03ce905cd7beea00a6e62dad0126
ashish-fsd <ashish.bansal@oncehub.com>
2020-02-25 18:45:52 +0530

    conflicts resolved

---
+++ product-market/frontend/src/app/sorting/sorting.component.ts
@@ -0,0 +1,31 @@
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTable } from '@angular/material/table';
+import { SortingDataSource, SortingItem } from './sorting-datasource';
+
+@Component({
+  selector: 'app-sorting',
+  templateUrl: './sorting.component.html',
+  styleUrls: ['./sorting.component.css']
+})
+export class SortingComponent implements AfterViewInit, OnInit {
+  @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: false}) sort: MatSort;
+  @ViewChild(MatTable, {static: false}) table: MatTable<SortingItem>;
+  datas: any;
+  dataSource: SortingDataSource;
+
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = [ 'name'];
+
+  ngOnInit() {
+    this.dataSource = new SortingDataSource();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.table.dataSource = this.dataSource;
+  }
+}
