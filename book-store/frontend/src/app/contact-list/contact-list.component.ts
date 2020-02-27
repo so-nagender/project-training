eeae5ab24503aff6990fab1d58fc0cb0342f0bae
monika-gupta <monika.gupta@oncehub.com>
2020-02-27 12:32:13 +0530

    sort

--- book-store/frontend/src/app/contact-list/contact-list.component.ts
+++ book-store/frontend/src/app/contact-list/contact-list.component.ts
@@ -65,4 +65,56 @@ export class ContactListComponent implements OnInit {
   
 
   }
+
+  bookNameSortAes (){ 
+    var table, i, x, y; 
+    table = document.getElementById("bookTable"); 
+    var switching = true; 
+    while (switching) { 
+        switching = false; 
+        var rows = table.rows; 
+      console.log(rows)
+        for (i = 1; i < (rows.length - 1); i++) { 
+            var Switch = false; 
+            x = rows[i].getElementsByTagName("td")[0]; 
+            y = rows[i + 1].getElementsByTagName("td")[0]; 
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) 
+                { 
+                Switch = true; 
+                break; 
+            }
+        } 
+        if (Switch) {  
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]); 
+            switching = true; 
+        } 
+    } 
+ }
+ 
+ bookNameSortDes(){ 
+  var table, i, x, y; 
+  table = document.getElementById("bookTable"); 
+  var switching = true; 
+  while (switching) { 
+      switching = false; 
+      var rows = table.rows; 
+    console.log(rows)
+      for (i = 1; i < (rows.length - 1); i++) { 
+          var Switch = false; 
+          x = rows[i].getElementsByTagName("td")[0]; 
+          y = rows[i + 1].getElementsByTagName("td")[0]; 
+          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) 
+              { 
+              Switch = true; 
+              break; 
+          }
+      } 
+      if (Switch) { 
+          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]); 
+          switching = true; 
+      } 
+  } 
+ }
+ 
+ 
 }
